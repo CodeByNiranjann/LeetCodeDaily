285cf8409741a220ea6115b15a308f5066d682c1
Niranjan <amuthanabishekniranjan@gmail.com>
2026-07-12 19:30:18 +0530

    Time: 31 ms (59.74%) | Memory: 77.1 MB (25.63%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int []sorted=arr.clone();
-        Arrays.sort(sorted);
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int rank=1;
+        int []sorted=arr.clone();  //clonign that arrays in sortes array
+        Arrays.sort(sorted);  //nxt sorting the sorted arrqy which contains the element in the sameas in arr
+        HashMap<Integer,Integer>map=new HashMap<>(); //creating hashmap store a valus and their rank
+        int rank=1;  //initialze the rank is 1
 
-        for(int i=0;i<sorted.length;i++){
+        for(int i=0;i<arr.length;i++){ 
             if(!map.containsKey(sorted[i])){
                 map.put(sorted[i],rank);
                 rank++;
